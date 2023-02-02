7af7bf26dd8ed99332f411c38e0963cb66382988
SANJEEV.R.K <69861510+sanjurk@users.noreply.github.com>
2023-02-03 00:30:07 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,367 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "mylist=[205,508,2670,120,301]",
+    question: "outpiut=[]",
+    question: "for num in mylist:",
+    question: "num=str(num)",
+    question: "newlist=num.split('0')",
+    question: "output.append(int(' ' .join(newlist)))",
+    question: "print(output)",
+    
+    answer: "[Hyper Text Markup Language]",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "mystr= 'a,a,a,a,b,b,c,c,c'",
+    question: "mylist=mystr.split(',')",
+    question: "visited=[]",
+    question: "final_list=[]",
+    question: "for ch in mylist:",
+    question: "if ch not in visited:",
+    question: "final_list.append(f'{ch}:{mylist.count(ch)}'}",
+    question: "visited.apppend(ch)",
+    question: "print(final_list)",
+    question: "print(','.join(final_list))",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "mylist=[1,2,2,3,3,4,5,5,5,6,6]",
+    question: "new_list=[]",
+    question: "for num in mylist:",
+    question: "if mylist.count(num)==1:",
+    question: "new_list.append(num)",
+    question: "print(nwe_list)",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "str1=:'Sky is blue'",
+    question: "mylist=str1.split()",
+    question: "mylist=mylist[::-1]",
+    question: "str2=''.join(mylist)",
+    question: "print(str2)",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "x=['a','b','c','d']",
+    question: "for x[-1] in x:",
+    question: "print(x[-1], end='')",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "def append(ele, to=[]):",
+    question: "to.append(ele)",
+    question: "return to",
+    question: " ",
+    question: "list_one = append(56)",
+    question: "print(list_one, end='')",
+    question: " ",
+    question: "list_two = append(23)",
+    question: "print(list_two)",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "var = {10,20,10,10,30,30,20}",
+    question: "print(len(var))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "var = 2e3",
+    question: "print(type(var))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "list1 = ['xyz', 222]",
+    question: "for i in list1:",
+    question: "list1.append(i)",
+    question: "print(i,end='')",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "deffunc(val1,val2=2,val3=7,val4=2):",
+    question: "return val1**val2**val3",
+    question: " ",
+    question: "print(func(val2=2,val1=2,val3=4))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Find out the exact output?",
+    question: "public class Main",
+    question: "{",
+    question: "public static void main(String[] args)",
+    question: "{",
+    question: "double data = 444.3254",
+    question: "int value = data;",
+    question: "System.out.println(data);",
+    question: "}",
+    question: "}",
+    
+    answer: "Error",
+    options: [
+      "444",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "mystr= 'a,a,a,a,b,b,c,c,c'",
+    question: "mylist=mystr.split(',')",
+    question: "visited=[]",
+    question: "final_list=[]",
+    question: "for ch in mylist:",
+    question: "if ch not in visited:",
+    question: "final_list.append(f'{ch}:{mylist.count(ch)}'}",
+    question: "visited.apppend(ch)",
+    question: "print(final_list)",
+    question: "print(','.join(final_list))",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "mylist=[1,2,2,3,3,4,5,5,5,6,6]",
+    question: "new_list=[]",
+    question: "for num in mylist:",
+    question: "if mylist.count(num)==1:",
+    question: "new_list.append(num)",
+    question: "print(nwe_list)",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 14,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "str1=:'Sky is blue'",
+    question: "mylist=str1.split()",
+    question: "mylist=mylist[::-1]",
+    question: "str2=''.join(mylist)",
+    question: "print(str2)",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 15,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "x=['a','b','c','d']",
+    question: "for x[-1] in x:",
+    question: "print(x[-1], end='')",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "def append(ele, to=[]):",
+    question: "to.append(ele)",
+    question: "return to",
+    question: " ",
+    question: "list_one = append(56)",
+    question: "print(list_one, end='')",
+    question: " ",
+    question: "list_two = append(23)",
+    question: "print(list_two)",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "var = {10,20,10,10,30,30,20}",
+    question: "print(len(var))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "var = 2e3",
+    question: "print(type(var))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "list1 = ['xyz', 222]",
+    question: "for i in list1:",
+    question: "list1.append(i)",
+    question: "print(i,end='')",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Find out the exact output?",
+    question: " ",
+    question: "deffunc(val1,val2=2,val3=7,val4=2):",
+    question: "return val1**val2**val3",
+    question: " ",
+    question: "print(func(val2=2,val1=2,val3=4))",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+
+    
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
